c3685980bb3a377b124a61705128d71104f7c6ea
Tetyana <alekstana@yahoo.ca>
2024-01-12 16:54:32 -0500

    Added iniFile

--- IniFile.py
+++ IniFile.py
@@ -4,7 +4,7 @@ import os
 
 class IniFile:
     def __init__(self):
-        self.path = 'TestStation.ini'
+        self.path = 'VcDvtTools.ini'
         if not os.path.exists(self.path):
             self.create_config()
 
@@ -14,65 +14,19 @@ class IniFile:
     def create_config(self):
         _iniParser = configparser.ConfigParser()
         """
-        Create section 'Hardware Server'
+        Create section 'Com port'
         """
-        _iniParser.add_section("Hardware Server")
-        _iniParser.set("Hardware Server", "host", "127.0.0.1")
-        _iniParser.set("Hardware Server", "port", "1883")
-        _iniParser.set("Hardware Server", "clientID", "")
-        """
-        Create section 'Authorization'
-        """
-        _iniParser.add_section("Authorization")
-        _iniParser.set("Authorization", "name", "")
-        _iniParser.set("Authorization", "password", "")
-
-        """
-        Create section 'Protocol'
-        """
-        _iniParser.add_section("Protocol")
-        _iniParser.set("Protocol", "type", "")
-
+        _iniParser.add_section("Com port")
+        _iniParser.set("Com port", "port", "Com 1")
 
         with open(self.path, "w") as f:
             _iniParser.write(f)
 
-    def get_hardwareServerIP(self):
-        return self.get_setting('Hardware Server', 'host')
+    def get_ComPort(self):
+        return self.get_setting('Com port', 'port')
 
-    def set_hardwareServerIP(self, serverIP):
-        self.update_setting('Hardware Server', 'host', serverIP)
-
-    def get_port(self):
-        return self.get_setting('Hardware Server', 'port')
-
-    def set_port(self, port):
-        self.update_setting('Hardware Server', 'port', port)
-
-    def get_clientID(self):
-        return self.get_setting('Hardware Server', 'clientID')
-
-    def set_clientID(self, clientID):
-        self.update_setting('Hardware Server', 'clientID', clientID)
-
-    def get_name(self):
-        return self.get_setting('Authorization', 'name')
-
-    def set_name(self, name):
-        self.update_setting('Authorization', 'name', name)
-
-    def get_password(self):
-        return self.get_setting('Authorization', 'password')
-
-    def set_password(self, password):
-        self.update_setting('Authorization', 'password', password)
-
-    def get_protocol(self):
-        return self.get_setting('Protocol', 'type')
-
-    def set_protocol(self, protocol):
-        self.update_setting('Protocol', 'type', protocol)
-
+    def set_ComPort(self, port):
+        self.update_setting('Com port', 'port', port)
 
     def get_setting(self, section, setting):
         _iniParser = configparser.ConfigParser()
